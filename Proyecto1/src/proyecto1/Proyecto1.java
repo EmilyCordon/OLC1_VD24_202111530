669a9d8cd697282aa97d26283e7a68e977affa50
EmilyCordon <124939253+EmilyCordon@users.noreply.github.com>
2024-12-08 21:11:59 -0600

    Add files via upload

---
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1;
+
+/**
+ *
+ * @author CLARO
+ */
+public class Proyecto1 {
+
+   
+    public static void main(String[] args) {
+        new Interfaz().setVisible(true);
+    }
+    
+}
